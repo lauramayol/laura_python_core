671bb58dda7b2d45344184d5cac5ac4621e970af
lauramayol <lmayol4@gmail.com>
2018-09-24 17:32:18 +0200

    feat: week 5 working on apis

---
+++ week_05/mini_projects/Follow_Birds_Project/followbirds.py
@@ -0,0 +1,66 @@
+'''
+Using the tweepy package, build a script that classifies a twitter handle
+into different groups according to the number of their followers.
+
+The classes can be whatever you like (e.g. I used ASCII art birds ;)
+
+CHALLENGE: Also fetch the number of their friends and display the ratio
+between followers and friends in an interesting way.
+
+'''
+import tweepy
+import os
+
+consumer_key = os.environ["TWITTER_API_KEY"]
+consumer_secret = os.environ["TWITTER_API_SECRET_KEY"]
+access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+access_token_secret = os.environ["TWITTER_ACCESS_SECRET"]
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+user_group = {"Over 1M": list(), "500K to 1M": list(), "50K to 500K": list(), "less than 50K": list()}
+print("We are searching people in Twitter. Please enter what topic you would like to search:")
+user_input = input()
+
+for u in tweepy.Cursor(api.search_users, q=(user_input)).items(400):
+    user_tuple = (u.screen_name, u.followers_count)
+    if u.followers_count > 1000000:
+        user_group["Over 1M"].append(user_tuple)
+    elif u.followers_count >= 500000:
+        user_group["500K to 1M"].append(user_tuple)
+    elif u.followers_count >= 50000:
+        user_group["50K to 500K"].append(user_tuple)
+    else:
+        user_group["less than 50K"].append(user_tuple)
+
+for key in user_group:
+    print(f"{key} users: ")
+    print(user_group[key])
+
+
+#public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
+
+# Get the User object for twitter...
+#user = api.get_user('twitter')
+
+# print(user.screen_name)
+# print(user.followers_count)
+# for friend in user.friends():
+#     print(friend.screen_name)
+
+# for tweet in tweepy.Cursor(api.search, q=('yoga')).items(5):
+#     print("Screen-name:", tweet.author.screen_name.encode('utf8'))
+#     print("Tweet created:", tweet.created_at)
+#     print("Tweet:", tweet.text.encode('utf8'))
+#     # print "Retweeted:", tweet.retweeted
+#     # print "Favourited:", tweet.favorited
+#     print("Location:", tweet.user.location.encode('utf8'))
+#     # print "Time-zone:", tweet.user.time_zone
+#     # print "Geo:", tweet.geo
+#     print(tweet.entities.get('hashtags'))
+#     print("//////////////////")
